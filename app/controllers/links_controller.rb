e32c4b04c0b27453baca366cc87568a6d756e073
Faith-Anne Kocadag <faith.anne@gmail.com>
2013-05-24 17:43:21 -0400

    Adding create and new to links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,14 @@
 class LinksController < ApplicationController
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(params[:link])
+    if @link.save
+      puts "Saved! URL is #{@link.user_url}"
+    else
+      puts "Error!"
+    end
+  end
 end
