284d75198b2cdba896f0a2c31b814878be3a9a54
Trevor Strieber <trevor@strieber.org>
2013-06-14 03:51:39 -0400

    removing http from front of urls before saving

--- app/models/link.rb
+++ app/models/link.rb
@@ -12,10 +12,13 @@
 
 class Link < ActiveRecord::Base
   attr_accessible :ip, :short_url, :user_url
-  before_save :generate_short_url
+  before_save :clean_and_generate_urls
 
   private
-  def generate_short_url
+  def clean_and_generate_urls
+    if self.user_url.start_with?('http://')
+      self.user_url = self.user_url.last(self.user_url.length - 7)
+    end
     self.short_url = Digest::MD5.hexdigest(self.user_url + DateTime.now.to_i.to_s).first(6).encode("UTF-8")
   end
 end
